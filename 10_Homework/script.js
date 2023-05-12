9c49cf855c944d09341dcc1609b3a36512b806c1
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-05-12 10:08:48 +0200

    update homework 10

--- 10_Homework/script.js
+++ 10_Homework/script.js
@@ -3,23 +3,31 @@
 // Add methods for depositing and withdrawing money from the account,
 // and a method for checking the balance.
 
+// модальное окно
+// alert('Hello!');
+
 const bankAccount = {
   accountNumber: 12,
   accountHolderName: "Peter",
   balance: 3500,
   deposit: function (sum) {
-    this.balance += sum;
+    sum > 60000
+      ? alert("Требуется подтверждение легальности доходов")
+      : (this.balance += sum);
     return `New balance after adding money is: ${this.balance}`;
   },
   withdrawalMoney: function (sum) {
-    this.balance -= sum;
+    sum <= this.balance
+      ? (this.balance -= sum)
+      : alert("Вы не можете снять сумму больше Вашего баланса");
     return `New balance after withdrawal is: ${this.balance}`;
   },
-  checkBalance: function (balance) {
-    return `Your balance is: ${balance}`;
+  checkBalance: function () {
+    return `Your balance is: ${this.balance}`;
   },
 };
 
-console.log(bankAccount.deposit(200));
-console.log(bankAccount.withdrawalMoney(400));
 console.log(bankAccount.checkBalance(bankAccount.balance));
+console.log(bankAccount.deposit(6600));
+console.log(bankAccount.withdrawalMoney(400));
+console.log(bankAccount.checkBalance());
