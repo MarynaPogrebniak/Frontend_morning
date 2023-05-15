2cec55944f01290477299c2b21371f6e694d2b83
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-05-15 16:55:25 +0200

    update

---
+++ 12_to_do_list_15_05_23/script.js
@@ -0,0 +1,28 @@
+const task = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTask");
+const taskListUl = document.getElementById("taskList");
+
+addTaskButton.addEventListener("click", addTask);
+taskListUl.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      if (e.target.textContent === "Done") {
+        const taskItem = e.target.parentElement;
+        const taskName = taskItem.querySelector("span");
+        taskName.style.textDecoration = "line-through red";
+      } else if (e.target.textContent === "Delete") {
+        console.log(e.target.parentElement.remove());
+      }
+    }
+  });
+
+function addTask() {
+  const taskName = task.value.trim();
+  if (taskName) {
+    const li = document.createElement("li");
+    li.innerHTML = `<span>${taskName}</span><button>Delete</button><button>Done</button>`;
+    taskListUl.appendChild(li);
+  }
+  task.value = "";
+}
+
+
