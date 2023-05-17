f0f316322d1c0a2193c0ca6556a9944e0cc97c42
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-05-17 10:20:55 +0200

    update homework

--- 12_to_do_list_15_05_23/script.js
+++ 12_to_do_list_15_05_23/script.js
@@ -3,8 +3,17 @@ const addTaskButton = document.getElementById("addTask");
 const taskListUl = document.getElementById("taskList");
 
 addTaskButton.addEventListener("click", addTask);
+// по Enter
+addTaskButton.addEventListener("keydown", e => {
+  console.log(e);
+  if (e.key === 'Enter') {
+    addTask;
+  }
+});
+
 taskListUl.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
+     
       if (e.target.textContent === "Done") {
         const taskItem = e.target.parentElement;
         const taskName = taskItem.querySelector("span");
@@ -12,13 +21,19 @@ taskListUl.addEventListener("click", (e) => {
       } else if (e.target.textContent === "Delete") {
         console.log(e.target.parentElement.remove());
       }
-    }
+    } // if(e.target.checked){
+          // e.target.previousSibling.style.textDecoration = "line-through black";
+      // } else {
+        // e.target.previousSibling.style.textDecoration = "none";
+      // }
   });
 
 function addTask() {
   const taskName = task.value.trim();
   if (taskName) {
     const li = document.createElement("li");
+    // li.innerHTML = `<span>${taskName}</span><input type="checkbox"><button>Delete</button><button>Done</button>`;
+
     li.innerHTML = `<span>${taskName}</span><button>Delete</button><button>Done</button>`;
     taskListUl.appendChild(li);
   }
